332a9a6d9a55800728b3ff892e7d8566c8a7b7cd
ahyiru <ah.yiru@gmail.com>
2022-04-06 11:47:43 +0800

    feat: update

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export {default as Anico} from './anico';
+export {ErrorBoundary,HandleError} from './errorBoundary';
+export {default as FullPage} from './fullPage';
+export {default as Row,Col} from './grid';
+export {default as Mask} from './mask';
+export {default as MaxSize} from './maxSize';
+export {default as Modal} from './modal';
+export {default as Panel} from './panel';
+export {default as Portal} from './portal';
+export {default as Spinner} from './spinner';
+export {default as TabHeader} from './tabHeader';
+
+// export {default as Sandbox,Modal,Dropdown,Notify,Message} from './draft/modal';
+
+export {default as fixEle} from './fixEle';
+export {default as loadError} from './loadError';
+export {default as str2React} from './str2React';
+export {default as str2Vue} from './str2Vue';
