71c2ddf510ed6ea2d1851c1d259151cd9684bf82
ahyiru <ah.yiru@gmail.com>
2026-08-06 12:36:08 +0800

    feat: update by ci

--- src/portal/index.js
+++ src/portal/index.js
@@ -1,10 +1,1 @@
-import { createPortal as __WEBPACK_EXTERNAL_MODULE_react_dom_20ef40a6_createPortal__ } from "react-dom";
-
-;// external "react-dom"
-
-;// ../huxy/components/portal/index.jsx
-
-const Portal = ({ children, mountNode = document.body }) => __WEBPACK_EXTERNAL_MODULE_react_dom_20ef40a6_createPortal__(children, mountNode);
-/* harmony default export */ const portal = (Portal);
-
-export { portal as default };
+import{createPortal as r}from"react-dom";var e=({children:o,mountNode:t=document.body})=>r(o,t),c=e;export{c as default};
